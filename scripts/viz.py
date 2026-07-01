277b596c34087223323fa46890e06f273a06cffa
Lev Dvorkin <lev_135@mail.ru>
2026-07-01 23:26:34 +0300

    Color support for nodes added

--- scripts/viz.py
+++ scripts/viz.py
@@ -4,10 +4,12 @@ import csv
 dot = Digraph(comment='Graph', format='png')
 dot.attr(rankdir='BT', nodesep='0.5')
 
+with open('out/node.csv', 'r') as f:
+    for name, color in csv.reader(f):
+        dot.node(name, name, color="black", fillcolor=color, style="filled")
+
 with open('out/edge.csv', 'r') as f:
     for src, dst in csv.reader(f):
-        dot.node(src, src)
-        dot.node(dst, dst)
         dot.edge(src, dst)
 
 dot.render('out/graph')  # Opens automatically
